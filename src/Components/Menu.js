4cc7eb574ec76d56b0bbabe1ad9901838fb4f60c
etozhenerk <etozhenerk@yandex.ru>
2021-02-02 23:38:31 +0300

    Созданы компоненты для меню

---
+++ src/Components/Menu.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import dbMenu from "./DBMenu";
+import { ListItem } from "./ListItem";
+import bannerImg from "../image/banner.png";
+
+const MenuStyled = styled.main`
+  background-color: #ccc;
+  margin-top: 80px;
+`;
+
+const SectionMenu = styled.section`
+  padding: 30px;
+`;
+
+const Banner = styled.img`
+  width: 100vw;
+  height: 210px;
+`;
+
+export const Menu = () => (
+  <MenuStyled>
+    <Banner src={bannerImg} alt="banner"/>
+    <SectionMenu>
+      <h2>Бургеры</h2>
+      <ListItem itemList={dbMenu.burger} />
+    </SectionMenu>
+    <SectionMenu>
+      <h2>Закуски / Напитки</h2>
+      <ListItem itemList={dbMenu.other} />
+    </SectionMenu>
+  </MenuStyled>
+);
