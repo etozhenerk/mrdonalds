7bac785190f3cdd5a9c86d6d08e4f396bed0fce7
etozhenerk <etozhenerk@yandex.ru>
2021-02-07 11:54:26 +0300

    -реорганизация папки компонентов

--- src/Components/Modal/ModalItem.js
+++ src/Components/Modal/ModalItem.js
@@ -46,14 +46,23 @@ const HeaderContent = styled.div`
   font-family: "Pacifico", cursive;
 `;
 
-export const ModalItem = ({ openItem, setOpenItem }) => {
+export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
+
   const closeModal = (e) => {
     if (e.target.id === "overlay") {
       setOpenItem(null);
     }
   };
 
-  if (!openItem) return null;
+  const order = {
+    ...openItem
+  };
+
+  const addToOrder = () => {
+    setOrders([...orders, order]);
+    setOpenItem(null);
+  }
+ 
   return (
     <Overlay id="overlay" onClick={closeModal}>
       <Modal>
@@ -68,7 +77,7 @@ export const ModalItem = ({ openItem, setOpenItem }) => {
               })}
             </div>
           </HeaderContent>
-          <ButtonCheckout>Добавить</ButtonCheckout>
+          <ButtonCheckout onClick={addToOrder}>Добавить</ButtonCheckout>
         </Content>
       </Modal>
     </Overlay>
--- src/Components/Order/OrderListItem.js
+++ src/Components/Order/OrderListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import trashImg from "../image/trash.svg";
+import trashImg from "../../image/trash.svg";
 
 const OrderItemStyled = styled.li`
   display: flex;
@@ -31,11 +31,12 @@ const TrashButton = styled.button`
   cursor: poiner;
 `;
 
-export const OrderListItem = () => (
+export const OrderListItem = ({ order }) => (
   <OrderItemStyled>
-    <ItemName>JS Burger</ItemName>
+    <ItemName>{order.name}</ItemName>
     <span>2</span>
-    <ItemPrice>750 Р</ItemPrice>
+    <ItemPrice>{order.price.toLocaleString('ru-Ru',
+        {style: 'currency', currency: 'RUB'})}</ItemPrice>
     <TrashButton/>
   </OrderItemStyled>
 )
